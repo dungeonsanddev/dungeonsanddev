1d1048b013ad814e587a942ab3294d99ad2f6ce9
Tejas Kumar <tejas@tejas.qa>
2023-03-10 16:15:36 +0100

    Update src/components/Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -124,7 +124,6 @@ export const Header = () => {
 
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 pt-2 pb-4">
-              {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
               <Disclosure.Button
                 as="a"
                 href="#"
