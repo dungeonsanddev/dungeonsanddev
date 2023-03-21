3d2019f572f1a87bec5df92bcfcb571f6022d3a8
Tejas Kumar <tejas@tejas.qa>
2023-03-21 13:52:20 +0100

    Handle null fallback case

--- src/pages/course/[slug]/index.tsx
+++ src/pages/course/[slug]/index.tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps } from 'next';
 import { getLessons, Lesson } from '~/utils/lessons-api';
 import { prisma } from '~/server/prisma';
 import SuperJSON from 'superjson';
@@ -9,18 +10,28 @@ import { Markdown } from '~/components/Markdown';
 import clsx from 'clsx';
 import { H1 } from '~/components/H1';
 
+type RealData = {
+  lessons: Lesson[];
+  course: CourseType;
+};
+
+type Props = {
+  data: string;
+};
+
 const Course = ({ data }) => {
-  const { course, lessons } = SuperJSON.parse(data) as {
-    course: CourseType;
-    lessons: Lesson[];
-  };
+  if (!data) {
+    return null;
+  }
+
+  const { course } = SuperJSON.parse<RealData>(data);
   return (
     <DefaultLayout>
-      <H1 className="font-cartdrige font-bold">{course.name}</H1>
+      <H1 className="font-bold font-cartdrige">{course.name}</H1>
       <div className={clsx('mt-8 flex items-start gap-8')}>
         <div className="grow shrink-0">
           <img
-            className="shrink-0 rounded-md"
+            className="rounded-md shrink-0"
             src={course.media}
             alt={course.name}
             width="300px"
@@ -29,7 +40,7 @@ const Course = ({ data }) => {
             href={`${course.slug}/buy`}
             className="mt-4 w-full inline-flex justify-center items-center gap-x-2 rounded-md bg-yellow-500 py-2.5 px-3.5 text-sm font-semibold text-gray-700 shadow-sm hover:bg-yellow-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-500"
           >
-            Get course
+            Buy course
             <ChevronDoubleRightIcon
               className="-mr-0.5 h-5 w-5"
               aria-hidden="true"
@@ -44,18 +55,23 @@ const Course = ({ data }) => {
   );
 };
 
-export const getServerSideProps = async (query) => {
+export const getServerSideProps: GetServerSideProps<Props> = async (query) => {
+  if (!query.params) {
+    return { notFound: true };
+  }
+
   const course = await prisma.course.findFirst({
     where: {
-      slug: query.params.slug,
+      slug: query.params.slug as string,
     },
   });
-  const lessons = await getLessons({ slug: query.params.slug });
+
+  const { lessons } = await getLessons({ slug: query.params.slug });
   const jsonString = SuperJSON.stringify({
     course,
     lessons,
   });
-  console.log(jsonString);
+
   return {
     props: { data: jsonString },
   };
